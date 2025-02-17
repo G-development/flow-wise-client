1f9328c47b2cb0d9b4756029eea4988230cfbeff
Giovanni Cuozzo <62568244+G-development@users.noreply.github.com>
2025-02-17 22:46:29 +0100

    Fix hydration problem

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,12 +27,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Analytics />
-      <SpeedInsights />
-      <Toaster />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
+        <SpeedInsights />
+        <Toaster />
         {children}
       </body>
     </html>
