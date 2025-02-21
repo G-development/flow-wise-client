885b4374ff1298a6ed93781164a62ea88ff31ce2
Giovanni Cuozzo <62568244+G-development@users.noreply.github.com>
2025-02-21 16:36:40 +0100

    New Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "@/hooks/useAuth";
 import { useFetch } from "@/hooks/useFetch";
-import { Pencil, Trash, Circle } from "lucide-react";
+import { Pencil, Trash, Circle, MoveUp, MoveDown } from "lucide-react";
 import { DateRange } from "react-day-picker";
 
 import {
@@ -36,16 +36,23 @@ interface Transaction {
   _id: string;
   user: string;
   amount: number;
-  category: string;
+  category: { _id: string; name: string };
   date: string;
   createdAt: string;
   updatedAt: string;
 }
 
+type Totals = {
+  income: number;
+  expense: number;
+};
+
 type DashboardData = {
   income?: Transaction[];
   expense?: Transaction[];
-  [key: string]: Transaction[] | undefined;
+  totals?: Totals;
+  net?: number;
+  savingsRate?: string;
 };
 
 export default function Dashboard() {
@@ -61,19 +68,13 @@ export default function Dashboard() {
     from: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
     to: new Date(),
   });
-  const { data, fetchData } = useFetch<DashboardData>("dashboard");
-  const { data: card1, fetchData: fD2 } = useFetch<{ totalAmount: number }>(
-    `income/total?startDate=${dateRange?.from}&endDate=${dateRange?.to}`
-  );
-  const { data: card2, fetchData: fD3 } = useFetch<{ totalAmount: number }>(
-    `expense/total?startDate=${dateRange?.from}&endDate=${dateRange?.to}`
+  const { data, fetchData } = useFetch<DashboardData>(
+    `dashboard?startDate=${dateRange?.from}&endDate=${dateRange?.to}`
   );
 
   useEffect(() => {
     fetchData();
-    fD2();
-    fD3();
-  }, [fetchData, fD2, fD3, dateRange]);
+  }, [fetchData, dateRange]);
 
   return (
     <>
@@ -93,8 +94,10 @@ export default function Dashboard() {
               </CardDescription>
             </CardHeader>
             <CardContent>
-              <p className="text-2xl font-bold">
-                {card1 ? "+" + card1.totalAmount + "€" : "No sum available"}
+              <p className="text-2xl font-bold text-green-500">
+                {data?.totals
+                  ? "+" + data.totals.income + "€"
+                  : "No sum available"}
               </p>
             </CardContent>
             <CardFooter className="text-sm">
@@ -110,8 +113,10 @@ export default function Dashboard() {
               </CardDescription>
             </CardHeader>
             <CardContent>
-              <p className="text-2xl font-bold">
-                {card2 ? "-" + card2.totalAmount + "€" : "No sum available"}
+              <p className="text-2xl font-bold text-red-600">
+                {data?.totals
+                  ? "-" + data.totals.expense + "€"
+                  : "No sum available"}
               </p>
             </CardContent>
             <CardFooter className="text-sm">
@@ -127,7 +132,14 @@ export default function Dashboard() {
               </CardDescription>
             </CardHeader>
             <CardContent>
-            <p className="text-2xl font-bold">Value - TBD</p>
+              <p className="text-2xl font-bold flex items-center">
+                {data?.net}€
+                {data?.net && data.net > 0 ? (
+                  <MoveUp className="text-green-500" />
+                ) : (
+                  <MoveDown className="text-red-500" />
+                )}
+              </p>
             </CardContent>
             <CardFooter className="text-sm">
               <p>Trend compared to last month</p>
@@ -140,7 +152,7 @@ export default function Dashboard() {
               <CardDescription>Percentage of income not spent</CardDescription>
             </CardHeader>
             <CardContent>
-            <p className="text-2xl font-bold">Value - TBD</p>
+              <p className="text-2xl font-bold">{data?.savingsRate}</p>
             </CardContent>
             <CardFooter className="text-sm">
               <p>Monthly savings goal: X%</p>
@@ -149,88 +161,90 @@ export default function Dashboard() {
         </div>
 
         <NewTransactionDrawer fetchData={fetchData} />
-        {data &&
-          Object.entries(data).map(([key, transactions]) => (
-            <Table key={key} className="caption-top">
-              <TableCaption>
-                A list of your recent {key} transactions.
-              </TableCaption>
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="w-[100px] capitalize">{key}</TableHead>
-                  <TableHead>Category</TableHead>
-                  <TableHead>Date</TableHead>
-                  <TableHead className="text-right">Amount</TableHead>
-                  <TableHead>Status</TableHead>
-                  <TableHead className="text-right">Actions</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {(transactions as Transaction[]).map((item) => (
-                  <TableRow key={item._id}>
-                    <TableCell className="font-medium capitalize">
-                      {key}
-                    </TableCell>
-                    <TableCell className="truncate max-w-[10ch] md:max-w-none">
-                      {item.category.charAt(0).toUpperCase() +
-                        item.category.slice(1)}
-                    </TableCell>
-                    <TableCell>
-                      {new Date(item.date).toLocaleDateString()}
-                    </TableCell>
-                    <TableCell
-                      className={`text-right ${
-                        key === "income" ? "text-green-500" : "text-red-500"
-                      }`}
-                    >
-                      {key === "income" ? "€" : "-€"}
-                      {item.amount}
-                    </TableCell>
-                    <TableCell className="text-center">
-                      <Circle
-                        className={`${getStatusColor(
-                          item.amount
-                        )} h-4 w-4 inline`}
-                      />
-                    </TableCell>
-                    <TableCell className="text-right flex justify-end gap-2">
-                      {/* Edit */}
-                      <Button
-                        size="sm"
-                        variant="outline"
-                        onClick={() => setTransactionToEdit(item._id)} // Imposta l'ID della transazione da eliminare
-                      >
-                        <Pencil className="h-4 w-4" />
-                      </Button>
-                      <EditDialog
-                        isOpen={transactionToEdit === item._id} // Mostra solo per la transazione selezionata
-                        onClose={() => setTransactionToEdit(null)} // Chiudi il dialogo
-                        transactionType={key}
-                        id={item._id}
-                        fetchData={fetchData}
-                      />
 
-                      {/* Delete */}
-                      <Button
-                        size="sm"
-                        variant="destructive"
-                        onClick={() => setTransactionToDelete(item._id)} // Imposta l'ID della transazione da eliminare
-                      >
-                        <Trash className="h-4 w-4" />
-                      </Button>
-                      <DeleteDialog
-                        isOpen={transactionToDelete === item._id}
-                        onClose={() => setTransactionToDelete(null)} // Chiudi il dialogo
-                        transactionType={key}
-                        id={item._id}
-                        fetchData={fetchData}
-                      />
-                    </TableCell>
+        {data &&
+          Object.entries(data).map(([key, transactions]) =>
+            Array.isArray(transactions) ? (
+              <Table key={key} className="caption-top">
+                <TableCaption>
+                  A list of your recent {key} transactions.
+                </TableCaption>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="w-[100px] capitalize">
+                      {key}
+                    </TableHead>
+                    <TableHead>Category</TableHead>
+                    <TableHead>Date</TableHead>
+                    <TableHead className="text-right">Amount</TableHead>
+                    <TableHead>Status</TableHead>
+                    <TableHead className="text-right">Actions</TableHead>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          ))}
+                </TableHeader>
+                <TableBody>
+                  {transactions.map((item) => (
+                    <TableRow key={item._id}>
+                      <TableCell className="font-medium capitalize">
+                        {key}
+                      </TableCell>
+                      <TableCell className="truncate max-w-[10ch] md:max-w-none">
+                        {item.category.name.charAt(0).toUpperCase() +
+                          item.category.name.slice(1)}
+                      </TableCell>
+                      <TableCell>
+                        {new Date(item.date).toLocaleDateString()}
+                      </TableCell>
+                      <TableCell
+                        className={`text-right ${
+                          key === "income" ? "text-green-500" : "text-red-500"
+                        }`}
+                      >
+                        {key === "income" ? "€" : "-€"}
+                        {item.amount}
+                      </TableCell>
+                      <TableCell className="text-center">
+                        <Circle
+                          className={`${getStatusColor(
+                            item.amount
+                          )} h-4 w-4 inline`}
+                        />
+                      </TableCell>
+                      <TableCell className="text-right flex justify-end gap-2">
+                        <Button
+                          size="sm"
+                          variant="outline"
+                          onClick={() => setTransactionToEdit(item._id)}
+                        >
+                          <Pencil className="h-4 w-4" />
+                        </Button>
+                        <EditDialog
+                          isOpen={transactionToEdit === item._id}
+                          onClose={() => setTransactionToEdit(null)}
+                          transactionType={key}
+                          id={item._id}
+                          fetchData={fetchData}
+                        />
+                        <Button
+                          size="sm"
+                          variant="destructive"
+                          onClick={() => setTransactionToDelete(item._id)}
+                        >
+                          <Trash className="h-4 w-4" />
+                        </Button>
+                        <DeleteDialog
+                          isOpen={transactionToDelete === item._id}
+                          onClose={() => setTransactionToDelete(null)}
+                          transactionType={key}
+                          id={item._id}
+                          fetchData={fetchData}
+                        />
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            ) : null
+          )}
       </div>
     </>
   );
