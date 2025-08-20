79cad249a1f77f2df76664a919f1c89ff5a47a2b
Giovanni Cuozzo <62568244+G-development@users.noreply.github.com>
2025-08-20 11:40:21 +0200

    +Categories route in navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -21,6 +21,7 @@ import {
   Upload,
   Banknote,
   Wallet,
+  Boxes,
 } from "lucide-react";
 import Logo from "../app/flow-wise-logo.svg";
 
@@ -108,6 +109,15 @@ export default function Navbar() {
               Wallets
             </Link>
 
+            <Link
+              href="/category"
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              prefetch={false}
+            >
+              <Boxes className="mr-2" />
+              Categories
+            </Link>
+
             <Separator className="my-4" />
 
             <Link
@@ -190,6 +200,13 @@ export default function Navbar() {
           prefetch={false}
         >
           Wallets
+        </Link>
+        <Link
+          href="/category"
+          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+          prefetch={false}
+        >
+          Categories
         </Link>
         <NavUser />
       </nav>
